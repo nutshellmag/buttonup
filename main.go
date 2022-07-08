7ab3cd49d08877d78e7472f34b1a13b8e289479b
Matthew Ronchetto <hello@doamatto.xyz>
2022-07-08 09:27:29 -0700

    chore: pushed headers to Caddy

--- main.go
+++ main.go
@@ -8,23 +8,6 @@ import (
 	"time"
 )
 
-func cors(fs http.Handler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Permission-Policy", "interest-cohort=(), browsing-topics=(), join-ad-interest-group=(), run-ad-auction=()")
-		w.Header().Set("X-Content-Type-Options", "nosniff")
-		w.Header().Set("X-Frame-Options", "DENY")
-		w.Header().Set("Referrer-Policy", "no-referrer")
-		w.Header().Set("Vary", "Origin")
-		w.Header().Set("Strict-Transport-Security", "max-age=31536000;includeSubDomains")
-		w.Header().Set("Cross-Origin-Opener-Policy", "same-origin")
-		w.Header().Set("Cross-Origin-Embedder-Policy", "require-corp")
-		// Comment the line below if being used in production outside of Nutshell
-		w.Header().Set("Content-Security-Policy", "default-src 'none'; style-src 'self'; img-src 'self' https://cdn.nutshellmag.com; font-src https://cdn.nutshellmag.com; form-action 'self'; upgrade-insecure-requests; block-all-mixed-content; object-src 'none'; connect-src 'none'; base-uri 'self'; frame-ancestors 'none'")
-
-		fs.ServeHTTP(w, r)
-	}
-}
-
 func subscribeUser(w http.ResponseWriter, r *http.Request) {
 	// Fetch Buttondown key
 	key := os.Getenv("BUTTONDOWN_KEY")
@@ -35,18 +18,6 @@ func subscribeUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	apiKey := []string{"Token",key}
-
-	// Set headers for security reason
-	w.Header().Add("Permission-Policy", "interest-cohort=(), browsing-topics=(), join-ad-interest-group=(), run-ad-auction=()")
-	w.Header().Set("X-Content-Type-Options", "nosniff")
-	w.Header().Set("X-Frame-Options", "DENY")
-	w.Header().Set("Referrer-Policy", "no-referrer")
-	w.Header().Set("Vary", "Origin")
-	w.Header().Set("Strict-Transport-Security", "max-age=31536000;includeSubDomains")
-	//w.Header().Set("Cross-Origin-Opener-Policy", "same-origin")
-	//w.Header().Set("Cross-Origin-Embedder-Policy", "require-corp")
-	// Comment the line below if being used in production outside of Nutshell
-	//w.Header().Set("Content-Security-Policy", "default-src 'none'; style-src 'self'; img-src 'self' https://cdn.nutshellmag.com; font-src https://cdn.nutshellmag.com; form-action 'self'; upgrade-insecure-requests; block-all-mixed-content; object-src 'none'; connect-src 'none'; base-uri 'self'; frame-ancestors 'none'")
 
 	// Fetch email from form data
 	if r.Header.Get("Content-Type") != "application/x-www-form-urlencoded" {
@@ -82,14 +53,14 @@ func subscribeUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	res.Body.Close()
-	http.Redirect(w, r, "/subscribed", http.StatusFound)
+	http.Redirect(w, r, "/subscribed.html", http.StatusFound)
 }
 
 func main() {
 	// Replace the below web/index.html, web/unsub.html, and web/subscribed.html
 	// with other files in this directory. You can also comment out these lines
 	// if you do not want to or need to serve them.
-	http.Handle("/", cors(http.FileServer(http.Dir("web/"))))
+	http.Handle("/", http.FileServer(http.Dir("web/")))
 
 	// This handles POST requests containing multipart/form-data. For an
 	// example, see the included web/ directory.
